1bdeb779756bdd92b5c4f30ae9d02508d57e0f53
DavidDupre <djdupre7@gmail.com>
2018-09-06 21:44:13 -0700

    Calibrate extruder with more control

--- utils/extruder_calibrator.py
+++ utils/extruder_calibrator.py
@@ -12,33 +12,57 @@ INCREMENT = .01
 
 
 def prompt_duty_cycle(duty_cycle_range):
-    stop = input('duty cycle: ({:0.4f}, {:0.4f}). Moving? '.format(duty_cycle_range[0], duty_cycle_range[1]))
-    print('\r', end='')
-    return stop == 'y'
+    return input('duty cycle: ({:0.4f}, {:0.4f}). Continue, (s)top, (r)everse, (i)ncrease delta, or (d)ecrease delta.'
+        .format(duty_cycle_range[0], duty_cycle_range[1]))
 
+
+def calibrate_direction(extruder, reverse=False):
+    if reverse:
+        start = RANGE
+        end = 0
+        delta = INCREMENT
+    else:
+        start = 0
+        end = RANGE
+        delta = -INCREMENT
+
+    while True:
+        # set extruder to move full speed in one direction using the current range
+        extruder._stop()
+        extruder._last_stopped_pos = start
+        extruder.set_position(end, 1)
+
+        # process input
+        prompt = prompt_duty_cycle(extruder._duty_cycle_range)
+        if prompt == 's':  # stop
+            extruder._stop()
+            break
+        elif prompt == 'r':  # reverse
+            temp = start
+            start = end
+            end = temp
+            delta = -delta
+        elif prompt == 'i':  # increase delta
+            delta *= 10
+            print('delta: {}'.format(delta))
+        elif prompt == 'd':  # decrease delta
+            delta *= .1
+            print('delta: {}'.format(delta))
+
+        # set next duty cycle range
+        if reverse:
+            extruder._duty_cycle_range = (extruder._duty_cycle_range[0], extruder._duty_cycle_range[1] + delta)
+        else:
+            extruder._duty_cycle_range = (extruder._duty_cycle_range[0] + delta, extruder._duty_cycle_range[1])
 
 def calibrate(pin):
     extruder = Extruder(gpio, pin, RANGE, (0, 1), 1)
 
     # calibrate for reverse direction (min duty cycle)
-    while extruder._duty_cycle_range[0] < extruder._duty_cycle_range[1]:
-        extruder._stop()
-        extruder._last_stopped_pos = RANGE
-        extruder.set_position(0, 1)
-        if prompt_duty_cycle(extruder._duty_cycle_range):
-            extruder._stop()
-            break
-        extruder._duty_cycle_range = (extruder._duty_cycle_range[0] + INCREMENT, extruder._duty_cycle_range[1])
+    calibrate_direction(extruder, reverse=True)
 
     # calibrate for forward direction (max duty cycle)
-    while extruder._duty_cycle_range[1] > extruder._duty_cycle_range[0]:
-        extruder._stop()
-        extruder._last_stopped_pos = 0
-        extruder.set_position(RANGE, 1)
-        if prompt_duty_cycle(extruder._duty_cycle_range):
-            extruder._stop()
-            break
-        extruder._duty_cycle_range = (extruder._duty_cycle_range[0], extruder._duty_cycle_range[1] - INCREMENT)
+    calibrate_direction(extruder, reverse=False)
 
     print('final duty cycle range: ({:0.4f}, {:0.4f})'.format(extruder._duty_cycle_range[0], extruder._duty_cycle_range[1]))
 
