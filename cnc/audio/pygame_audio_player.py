45b99163ff8e1fd48b90d5fb84a6451cac2dda71
DavidDupre <djdupre7@gmail.com>
2018-09-13 22:59:29 -0700

    Handle device not found error on mixer init

--- cnc/audio/pygame_audio_player.py
+++ cnc/audio/pygame_audio_player.py
@@ -1,9 +1,17 @@
+import pygame
 import pygame.mixer as mixer
 
 from cnc.audio.base_audio_player import BaseAudioPlayer
 
 
-mixer.init()
+_initialized = False
+try:
+    mixer.init()
+    _initialized = True
+except pygame.error as e:
+    # catch the error for when no device is detected
+    # this can happen when a bluetooth device is not connected, for example
+    print(e)
 
 class PygameAudioPlayer(BaseAudioPlayer):
     """
@@ -14,10 +22,14 @@ class PygameAudioPlayer(BaseAudioPlayer):
 
     @classmethod
     def play(cls, filepath):
+        if not _initialized:
+            return
         if filepath is not cls._prev_filepath:
             mixer.music.load(filepath)
         mixer.music.play()
 
     @classmethod
     def stop(cls):
+        if not _initialized:
+            return
         mixer.music.stop()
